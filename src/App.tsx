2be11945e3a273d1330cfe9e17394d7df7c46c73
Maryna Mazur <marinemazur@gmail.com>
2023-01-24 18:32:57 +0200

    created app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,125 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {
+  useCallback,
+  useContext,
+  useEffect,
+  useMemo,
+  useState,
+} from 'react';
+import { ThreeDots } from 'react-loader-spinner';
+import { Route, Routes } from 'react-router-dom';
+import { getTodos } from './api/todos';
+import { AuthContext } from './components/Auth/AuthContext';
+import { ContextTextError } from './components/Context/ContextTextError';
+import { ContextTodos } from './components/Context/ContextTodos';
+import { ErrorNotifications } from './components/ErrorNotifications';
+import { HeaderForm } from './components/HeaderForm';
+import { TodoFilter } from './components/TodosFilter';
+import { ErrorType } from './types/ErrorType';
 
-function App() {
+export const App: React.FC = () => {
+  const { user } = useContext(AuthContext);
+  const { textError, setTextError } = useContext(ContextTextError);
+  const { todos, setTodos, isAddingTodo } = useContext(ContextTodos);
+  const { setUser } = useContext(AuthContext);
+
+  const userId = user?.id || 0;
+  const unCompletedTodos = useMemo(() => {
+    return [...todos].filter(todo => !todo.completed);
+  }, [todos]);
+  const completedTodos = useMemo(() => {
+    return [...todos].filter(todo => todo.completed);
+  }, [todos]);
+
+  const [isLoadingTodos, setIsLoadingTodos] = useState(false);
+
+  const loaderTodos = useCallback(async () => {
+    try {
+      const todosFromServer = await getTodos(userId);
+
+      setTodos(todosFromServer);
+    } catch {
+      setTextError(ErrorType.GET);
+    } finally {
+      setIsLoadingTodos(false);
+    }
+  }, [userId]);
+
+  const handlerClearLocalStorage = useCallback(() => {
+    localStorage.clear();
+    setUser(null);
+  }, []);
+
+  useEffect(() => {
+    setIsLoadingTodos(true);
+    loaderTodos();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="todoapp">
+      <div className="todoapp__log-out">
+        <span className="icon-text">
+          <span className="icon">
+            <i className="fas fa-user is-small" />
+          </span>
+          <span>{`Hello, ${user?.name}`}</span>
+        </span>
+        <button
+          type="button"
+          className="button is-outlined is-rounded"
+          onClick={handlerClearLocalStorage}
         >
-          Learn React
-        </a>
-      </header>
+          Log out
+        </button>
+      </div>
+      <h1 className="todoapp__title">todos</h1>
+
+      <div className="todoapp__content">
+        <HeaderForm
+          unCompletedTodos={unCompletedTodos}
+        />
+
+        {isLoadingTodos && (
+          <div className="todoapp__loader">
+            <ThreeDots
+              height="80"
+              width="80"
+              radius="9"
+              color="#af2f2f75"
+              ariaLabel="three-dots-loading"
+              visible
+            />
+          </div>
+        )}
+
+        {(todos?.length || isAddingTodo) && (
+          <Routes>
+            <Route
+              path="/"
+              element={(
+                <TodoFilter
+                  unCompletedTodos={unCompletedTodos}
+                  completedTodos={completedTodos}
+                />
+              )}
+            />
+
+            <Route
+              path="/:filter"
+              element={(
+                <TodoFilter
+                  unCompletedTodos={unCompletedTodos}
+                  completedTodos={completedTodos}
+                />
+              )}
+            />
+          </Routes>
+
+        )}
+      </div>
+
+      {textError && (
+        <ErrorNotifications />
+      )}
     </div>
   );
-}
-
-export default App;
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createRoot } from 'react-dom/client';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import 'bulma/css/bulma.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+import './styles/index.scss';
+
+import { HashRouter } from 'react-router-dom';
+import { App } from './App';
+import { AuthProvider } from './components/Auth/AuthContext';
+import { TextErrorProvider } from './components/Context/ContextTextError';
+import { ContextTodosProvider } from './components/Context/ContextTodos';
+import {
+  ContextToggleAllProvider,
+} from './components/Context/ContextToggleAll';
+
+const Root = () => (
+  <HashRouter>
+    <AuthProvider>
+      <TextErrorProvider>
+        <ContextTodosProvider>
+          <ContextToggleAllProvider>
+            <App />
+          </ContextToggleAllProvider>
+        </ContextTodosProvider>
+      </TextErrorProvider>
+    </AuthProvider>
+  </HashRouter>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+createRoot(document.getElementById('root') as HTMLDivElement).render(<Root />);
